c20fcc8f30e89dae6f069bd983331e8bc74692a0
Pushparajkumar <rajpush1961@gmail.com>
2016-09-02 14:22:34 +0530

    Update vo.java

--- vo.java
+++ vo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class CamelCase {
+public class CamelCaseraj {
  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
